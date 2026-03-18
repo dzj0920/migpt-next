91965b638e7be30effe04e8dd84256a082b4e02c
dzj0920 <73095586+dzj0920@users.noreply.github.com>
2026-03-18 16:57:43 +0800

    Update config.js

--- apps/example/config.js
+++ apps/example/config.js
@@ -9,25 +9,25 @@ export default {
      *
      * 如果提示找不到设备，请打开调试模式获取设备真实的 name、miotDID 或 mac 地址填入
      */
-    did: 'Xiaomi 智能音箱 Pro',
+    did: '小爱音箱mini',
     /**
      * 小米 ID（一串数字）
      *
      * 注意：不是手机号或邮箱，请在小米账号「个人信息」-「小米 ID」查看
      */
-    userId: '1234567',
+    userId: '283181252',
     /**
      * 小米账号登录密码
      *
      * 如果提示登录失败，请使用 passToken 登录
      */
-    password: 'xxxxx',
+    password: 'dzj930920',
     /**
      * （可选）小米账号 passToken
      *
      * 获取教程：https://github.com/idootop/migpt-next/issues/4
      */
-    passToken: 'xxxxxxxxx',
+    passToken: 'V1:pwROAesIuzMBPe5slLSsQnUb2DpHBc+t6DtAMHLpye9bq66oUqU42OclC/Vx0WLuaKSnZYKK+Urck/q3kgc7Sfh7ZAGYa5xD5rdZ8MjtoOoungWx9BCfJQvgzl0f0UX0mB7IZS5yz4sSIT+FW+fW+1rH0VW+ekDuqgAFwkXcueFpswGgV1d9gy/+Kl0Jtum7+6gZJixIOUwq92SNffXmpj7t65bT8q1V4vvTfaHNKbAT6xysQL3t4m5PwU01KAd7Dhg63nsJMt4FGbG6juI9CQ3XSkBHLk0oEkzybJ7zU7J7JHIMrGZezfhz4MYq88oTQ6Dus86pqdvyMj4BSsDm1g==',
   },
   openai: {
     /**
@@ -40,15 +40,15 @@ export default {
      * - ❌ https://api.openai.com/v1/（最后多了一个 /
      * - ❌ https://api.openai.com/v1/chat/completions（不需要加 /chat/completions）
      */
-    baseURL: 'https://api.openai.com/v1',
+    baseURL: 'https://ark.cn-beijing.volces.com/api/v3',
     /**
      * API 密钥
      */
-    apiKey: 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    apiKey: '600ab1ab-5a05-4462-b817-4b31d261846c',
     /**
      * 模型名称
      */
-    model: 'gpt-4.1-mini',
+    model: 'ep-20260315144626-zbwf6',
   },
   prompt: {
     /**
